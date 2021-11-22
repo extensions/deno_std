70a20d8def9daf72a3377b52ef59d33b38611cea
Jeremy <_@jeremy.ca>
2021-11-22 04:04:47 +0000

    testing

--- testing/asserts_static_test.ts
+++ testing/asserts_static_test.ts
@@ -1,57 +1,39 @@
 import { assertStatic, TypeEquals } from "./asserts_static.ts";
-import { assertEquals } from "./asserts.ts";
-import { writeAll } from "../io/util.ts";
 import { dirname, fromFileUrl } from "../path/mod.ts";
+import { _assertTypescriptErrors } from "../../../x/deno_std/testing/asserts.ts";
 
 const moduleDir = dirname(fromFileUrl(import.meta.url));
 
-// SUB-TASK:
-// implement that nice system for testing type errors!
-
-const typeErrorsTest = (code: string, expectedErrors: string) =>
-  async () => {
-    const p = Deno.run({
-      cmd: [
-        Deno.execPath(),
-        "run",
-        "--quiet",
-        "-",
-      ],
-      env: {
-        "NO_COLOR": "NO_COLOR",
-      },
-      cwd: moduleDir,
-      stdin: "piped",
-      stdout: "null",
-      stderr: "piped",
-    });
-
-    await writeAll(
-      p.stdin,
-      new TextEncoder().encode(`
-        import { assertStatic, TypeEquals, TypeExtends, TypeStrictlyExtends } from "./asserts_static.ts";
-      ${code.trim()}
-      `),
-    );
-    p.stdin.close();
-    const output = await p.stderrOutput();
-    p.close();
-    const actualErrors = new TextDecoder().decode(output).replaceAll(
-      /file:\/\/.*?\$deno\$stdin\.ts/g,
-      "",
-    );
-    const normalize = (s: string) => s.trim().replace(/[ \t]*\n+[ \t]*/g, "\n");
-    assertEquals(normalize(actualErrors), normalize(expectedErrors));
-  };
-
 Deno.test(
-  "some successes",
+  "assertStatic/TypeEquals/success",
   () => {
     assertStatic<TypeEquals<2, 2 & number>>();
-    assertStatic<TypeEquals<3 & (number | string), 2>>();
+    assertStatic<TypeEquals<2 & (number | string), 2>>();
     const x = "hello";
-    assertStatic<TypeEquals<typeof x, "sfage">>();
+    assertStatic<TypeEquals<typeof x, "hello">>();
     assertStatic<TypeEquals<"hello", "hello">>();
+  },
+);
+
+Deno.test(
+  "assertStatic/TypeEquals/failure",
+  async () => {
+    await _assertTypescriptErrors(import.meta, (ts) =>
+      ts /* ts */`
+      import { assertStatic, TypeEquals } from "./asserts_static.ts";
+
+      assertStatic<TypeEquals<2, 2 & number>>();
+      assertStatic<TypeEquals<3 & (number | string), 2>>();
+      ${`
+        TS2344 [ERROR]: Type 'Failure<["Expected left type to exactly match right type ", 2, " but left type was ", 3, ". "]>' does not satisfy the constraint '"static assertions must pass"'.
+      `}
+      const x = "hello";
+      assertStatic<TypeEquals<typeof x, "goodbye">>();
+      ${`
+        TS2344 [ERROR]: Type 'Failure<["Expected left type to exactly match right type ", "goodbye", " but left type was ", "hello", ". "]>' does not satisfy the constraint '"static assertions must pass"'.
+      `}
+      assertStatic<TypeEquals<"hello", "hello">>();
+  `);
   },
 );
 
