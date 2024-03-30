deca4a6a460b0517a5558852808c03c7ae6caef6
Jeremy Banks <_@jeremy.ca>
2024-03-30 16:10:37 -0400

    bring back the junk

--- crypto/_wasm/src/fnv.rs
+++ crypto/_wasm/src/fnv.rs
@@ -1,12 +1,25 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+use digest::core_api::BlockSizeUser;
+
 const BASIS_32: u32 = 2166136261;
 const PRIME_32: u32 = 16777619;
 
 const BASIS_64: u64 = 14695981039346656037;
 const PRIME_64: u64 = 1099511628211;
 
+#[derive(Debug, Clone)]
 pub struct Fnv32 {
   state: u32,
+}
+
+impl Default for Fnv32 {
+  fn default() -> Fnv32 {
+    Fnv32::new()
+  }
+}
+
+impl BlockSizeUser for Fnv32 {
+  type BlockSize = typenum::U1;
 }
 
 impl Fnv32 {
@@ -26,8 +39,19 @@ impl Fnv32 {
   }
 }
 
+#[derive(Debug, Clone)]
 pub struct Fnv32A {
   state: u32,
+}
+
+impl Default for Fnv32A {
+  fn default() -> Fnv32A {
+    Fnv32A::new()
+  }
+}
+
+impl BlockSizeUser for Fnv32A {
+  type BlockSize = typenum::U1;
 }
 
 impl Fnv32A {
@@ -47,8 +71,19 @@ impl Fnv32A {
   }
 }
 
+#[derive(Debug, Clone)]
 pub struct Fnv64 {
   state: u64,
+}
+
+impl BlockSizeUser for Fnv64 {
+  type BlockSize = typenum::U1;
+}
+
+impl Default for Fnv64 {
+  fn default() -> Fnv64 {
+    Fnv64::new()
+  }
 }
 
 impl Fnv64 {
@@ -68,8 +103,19 @@ impl Fnv64 {
   }
 }
 
+#[derive(Debug, Clone)]
 pub struct Fnv64A {
   state: u64,
+}
+
+impl BlockSizeUser for Fnv64A {
+  type BlockSize = typenum::U1;
+}
+
+impl Default for Fnv64A {
+  fn default() -> Fnv64A {
+    Fnv64A::new()
+  }
 }
 
 impl Fnv64A {
